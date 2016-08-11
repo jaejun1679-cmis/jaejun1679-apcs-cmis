73a101a7f50020c56ff00aaf8594b7665f8f5556
cmis <cmis@cmis-hs18.(none)>
2016-08-11 08:40:38 +0700

    Finishing touches.

--- GettingStarted/AddNumber.java
+++ GettingStarted/AddNumber.java
@@ -9,17 +9,29 @@ public class AddNumber{
     
     int number1 = Integer.parseInt( numberA );
     int number2 = Integer.parseInt( numberB );
-        
+    
     int Add = number1 + number2;
     int Sub = number1 - number2;
     int Div = number1 / number2;
     int Mul = number1 * number2;
     int MDiv = number1 % number2;
     
+    int Add2 = number2 + number1;
+    int Sub2 = number2 - number1;
+    int Div2 = number2 / number1;
+    int Mul2 = number2 * number1;
+    int MDiv2 = number2 % number1;
+    
     System.out.println(Integer.toString(number1) + "+" + Integer.toString(number2) + "=" + Integer.toString (Add));
     System.out.println(Integer.toString(number1) + "-" + Integer.toString(number2) + "=" + Integer.toString (Sub));
     System.out.println(Integer.toString(number1) + "/" + Integer.toString(number2) + "=" + Integer.toString (Div));
     System.out.println(Integer.toString(number1) + "*" + Integer.toString(number2) + "=" + Integer.toString (Mul));
     System.out.println(Integer.toString(number1) + "%" + Integer.toString(number2) + "=" + Integer.toString (MDiv));
+    
+    System.out.println(Integer.toString(number2) + "+" + Integer.toString(number1) + "=" + Integer.toString (Add2));
+    System.out.println(Integer.toString(number2) + "-" + Integer.toString(number1) + "=" + Integer.toString (Sub2));
+    System.out.println(Integer.toString(number2) + "/" + Integer.toString(number1) + "=" + Integer.toString (Div2));
+    System.out.println(Integer.toString(number2) + "*" + Integer.toString(number1) + "=" + Integer.toString (Mul2));
+    System.out.println(Integer.toString(number2) + "%" + Integer.toString(number1) + "=" + Integer.toString (MDiv2));
   } 
 }
