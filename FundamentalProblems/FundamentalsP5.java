6dc02c8dda5a7c64398396a4939ce1cb01aaa9de
cmis <cmis@cmis-hs04.(none)>
2016-08-19 09:55:15 +0700

    Stuck on FundamentalsP5.

---
+++ FundamentalProblems/FundamentalsP5.java
@@ -0,0 +1,15 @@
+public class FundamentalsP5
+{
+    public static void main ( String args [] ) 
+    {
+      String [] favoritemovies = new String [3];
+      
+      favoritemovies [0] = "The Silence of the Lambs";
+      favoritemovies [1]= "Se7en";
+      favoritemovies [2] = "Zootopia";
+      
+      for( int [0] ){
+          
+        } // end for     
+    }// end method main
+}//end class
