63c12ca56085256b62bdec883aaf7445d36e6d9f
cmis <cmis@cmis-hs04.(none)>
2016-08-22 11:55:30 +0700

    Gave up on 5. Started 6.

--- FundamentalProblems/FundamentalsP5.java
+++ FundamentalProblems/FundamentalsP5.java
@@ -2,14 +2,28 @@ public class FundamentalsP5
 {
     public static void main ( String args [] ) 
     {
-      String [] favoritemovies = new String [3];
+       String [] favoritemovies = new String [3];      
+       favoritemovies [0] = "The Silence of the Lambs";
+       favoritemovies [1]= "Se7en";
+       favoritemovies [2] = "Zootopia";
       
-      favoritemovies [0] = "The Silence of the Lambs";
-      favoritemovies [1]= "Se7en";
-      favoritemovies [2] = "Zootopia";
+       String [] favoritesongs = new String [3];      
+       favoritesongs [0] = "Light It Up - Major Lazer";
+       favoritesongs [1] = "Hotline Bling - Drake ";
+       favoritesongs [2] = "Under Pressure - Logic";
       
-      for( int [0] ){
-          
-        } // end for     
-    }// end method main
-}//end class
+      
+       for ( int index = 0; index < favoritemovies.length; index++ )
+       {
+         System.out.println ( favoritemovies[index] );        
+       } // end for
+       
+       int count = 0;
+       
+       while ( count = 0); index2 < favoritesongs.length; index2++ )
+       {
+        System.out.println ( favoritesongs[index] );
+        }// end while
+    } // end method main     
+}// end class
+
---
+++ FundamentalProblems/FundamentalsP6.java
@@ -0,0 +1,12 @@
+
+public class FundamentalsP6
+{
+    public static void main ( String [] args )
+    {
+        int [] numbertray = new int[50];
+        
+        for ( int index = 0; index < numbertray.length; index ++) {
+         Math.random() double arraynumbers = Math.random()*100 ; 
+        }// end for             
+    }// end method main
+}// end class
