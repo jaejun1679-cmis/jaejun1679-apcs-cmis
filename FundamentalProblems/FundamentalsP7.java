85c3ca6fccf18efd8ac59d7b821d2997fe3470e6
cmis <cmis@cmis-hs04.(none)>
2016-08-24 08:42:57 +0700

    Work day.

--- FundamentalProblems/FundamentalsP7.java
+++ FundamentalProblems/FundamentalsP7.java
@@ -2,15 +2,18 @@ public class FundamentalsP7
 {
     public static void main ( String [] args )
     {
-        int [] numbertray = new int[10];
-        
-        for ( int index = 0; index < numbertray.length; index ++ ) 
+        int [] numbertray = new int[10];        
+        for ( int index = 0; index < 10; index ++ ) 
         {
             double value = Math.random() * 100;
             numbertray[ index ] =  (int) value;
-            System.out.println( numbertray[ index ] );
-            
-            
         }// end for 
+        
+        int count = 0;
+        while( count < numbertray.length )
+        {
+            System.out.print ( numbertray [ count ] + " ");
+            count++;        
+        }//end while
     }// end method main
 }// end class
--- FundamentalProblems/test.java
+++ FundamentalProblems/test.java
@@ -1,5 +1,12 @@
-class test {
-    public static void main(String[] args){
-        
-        }
+class test
+{
+    public static void main(String[] args)
+    {
+        int [] nums = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        for (int index = 0; index < nums.length; index--)
+        {
+            int thisNum = nums[ index ];
+            System,out.println( thisNum * thisNum );
+        }    
     }
+}
