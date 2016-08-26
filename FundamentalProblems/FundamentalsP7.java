30d402f67da5ff1404659e2ea2d4a295929ffc13
cmis <cmis@cmis-hs04.(none)>
2016-08-26 11:53:50 +0700

    Almost done with FundamentalsP7. I will wrap it up over the weekend.

--- FundamentalProblems/FundamentalsP7.java
+++ FundamentalProblems/FundamentalsP7.java
@@ -2,18 +2,22 @@ public class FundamentalsP7
 {
     public static void main ( String [] args )
     {
-        int [] numbertray = new int[10];        
-        for ( int index = 0; index < 10; index ++ ) 
+       int [] numbertray = new int[10];      
+       
+       for ( int index = 0; index < 10; index ++ ) 
         {
-            double value = Math.random() * 100;
-            numbertray[ index ] =  (int) value;
-        }// end for 
-        
-        int count = 0;
-        while( count < numbertray.length )
+         double value = Math.random() * (100 - (-100) +1) - 100;
+         numbertray[ index ] =  (int) value;
+         
+         System.out.print( numbertray[index] + " ");
+       } 
+       
+       for ( int index = 0; index < 10; index ++ ) 
         {
-            System.out.print ( numbertray [ count ] + " ");
-            count++;        
-        }//end while
-    }// end method main
-}// end class
+         if( numbertray[index] > 0)
+         {
+             System.out.print( numbertray [index] + " ");
+         }
+       } 
+    }
+}
