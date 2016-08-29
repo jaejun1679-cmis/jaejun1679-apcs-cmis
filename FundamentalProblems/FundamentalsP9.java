3a28dc36e2c59de2611364844d541517c9f826ff
cmis <cmis@cmis-hs04.(none)>
2016-08-29 14:35:25 +0700

    Why must binary be so hard?

--- FundamentalProblems/FundamentalsP9.java
+++ FundamentalProblems/FundamentalsP9.java
@@ -4,26 +4,21 @@ public class FundamentalsP9 {
     {
         String username = JOptionPane.showInputDialog( "Please create a username: ");
         String stringpassword = JOptionPane.showInputDialog( "Please create a password: ");
-        
+
         int intpassword = Integer.parseInt( stringpassword );
-        
-        String loginyesorno = JOptionPane.showInputDialog( "Would you like to login? (Yes or No) ");
-        
-        if (loginyesorno.equals("Yes"))
+
+        String loginusername = JOptionPane.showInputDialog( "Please insert you username: ");
+        String loginpassword = JOptionPane.showInputDialog( "Please insert you password: ");
+
+        int intloginpassword = Integer.parseInt( loginpassword );
+
+        if(username.equals(loginusername) && intpassword == intloginpassword)
         {
-            String loginusername = JOptionPane.showInputDialog( "Please insert you username: ");
-            String loginpassword = JOptionPane.showInputDialog( "Please insert you password: ");
-            
-            int intloginpassword = Integer.parseInt( loginpassword );
-            
-            if(username == loginusername && intpassword == intloginpassword)
-            {
-                System.out.println( "You have successfully logged in");
-            }
-            else
-            {
-                System.out.println ( "Login unsuccessful…try again");
-            }//end 2nd if
-        }// end if
-    }// end method main
-}// end class
+            System.out.println( "You have successfully logged in");
+        }
+        else
+        {
+            System.out.println ( "Login unsuccessful…try again");
+        }
+    }
+}
