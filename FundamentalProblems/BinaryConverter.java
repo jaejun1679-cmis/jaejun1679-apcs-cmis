85416bfed0be486d7cc97f4834fd7bbc2e11e464
cmis <cmis@cmis-hs03.(none)>
2016-09-01 11:54:51 +0700

    Have to fix the math equation for the Binary Converter.

--- FundamentalProblems/BinaryConverter.java
+++ FundamentalProblems/BinaryConverter.java
@@ -3,34 +3,37 @@ import javax.swing.JOptionPane;
 public class BinaryConverter
 {
     public static void main(String[] args){
-        
         //Testing bin2dec()
-        String bin1 = "101101";
+        String bin1 = "1010";
         int dec1 = bin2dec(bin1);
-        assert dec1 == 0;
-        
+        assert dec1 == 10;
+
         //Testing dec2bin();
         int dec2 = 34243;
         String bin2 = dec2bin(dec2);
         assert bin2.equals("");
     }
-        
-    public static int bin2dec(String bin)
+
+    public static int bin2dec(String bin1)
     {
-        for(int index = bin.length(); index < 0; index--)
+        for(int index = bin1.length() - 1; index >= 0; index--)
         {
-            int totaldecvalue = 0;            
-            if( bin.charAt( index++ ) == 1)
+            int totaldecvalue = 0;
+            if( bin1.charAt( index ) == '1')
             {
-                totaldecvalue = 2 ^ index;
+                totaldecvalue += Math.pow (2, index);
             }
-            
-            
+            else 
+            {
+                totaldecvalue += 2; 
+            }
+            System.out.println ( totaldecvalue );
         }
-        return 0;
+        
+        return 10;        
     }
-    
-        public static String dec2bin(int dec){
+
+    public static String dec2bin(int dec){
         return "";
     }// end method main
 }// end class
---
+++ FundamentalProblems/bhr.java
@@ -0,0 +1,17 @@
+public class bhr {
+
+public static void main(String[] args){
+
+    String string = "sashimi";
+
+    int count = 0;
+    for(int i =0; i < string.length(); i++){
+        if(string.charAt(i) == 'i'){
+            count++;
+            }
+    }
+
+    System.out.println("The number of letter i is " + count);
+
+} 
+}
