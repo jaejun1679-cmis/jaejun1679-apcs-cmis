1010b6e4ba0f2299db291994f70db5583d4e8b7d
JJ Choi <JJ Choi>
2016-09-14 23:17:55 +0700

    Worked on the BigProblem1 at home.

--- BigProblem1/Histogram.java
+++ BigProblem1/Histogram.java
@@ -5,29 +5,49 @@ public class Histogram {
         String userinput = JOptionPane.showInputDialog( "Insert any number: "); //userinput number
         int intuserinput = Integer.parseInt( userinput ); //convert userinput to int
 
-        int [] numberrange = new int [99]; //defined first array
-        int max = 0, min = 0, mode = 0, avg = 0, sum = 0;
+        int [] numberrange = new int [100]; //defined first array
 
-        for (int index = 0; index <= intuserinput; index++) 
+        for (int index = 0; index < intuserinput; index++) 
         //Generate numbers and put in array
         {
-            int numbergenerated = (int) (Math.random() * 100);
+            int numbergenerated = (int)(Math.random() * 100);
             numberrange [numbergenerated] += 1;
         }//end first for loop
 
-        
         for (int index = 0; index < 100; index++) //Print array with correct #'#'
         {
             String hasnumbers = "";
-            
-            /*
-            for (int index2 = 0; index2 < numberrange.length; index++)
+            while(numberrange[index] > 0)
             {
-                
+                hasnumbers += "#";
+                numberrange[index]--;
             }
-            */
             System.out.println( index + ": " + hasnumbers);
-                        
         }//end second for loop
-    }//end method main
-}//end class
+
+        int max = 0, min = 99, avg = 0, sum = 0, mode = 0;
+
+        for (int index = 0; index < numberrange.length; index++)
+        //finding the max and the min
+        {
+            if ( numberrange[ index ] >= max )
+            {
+                max = numberrange[index];
+            }
+            if ( numberrange[ index ] <= min )
+            {
+                min = numberrange[index];
+            }
+        }
+        System.out.println( "Maximum: " + max);
+        System.out.println( "Minimum: " + min);
+        
+        for (int index = 0; index < numberrange.length; index++)
+        //finding the sum
+        {
+            sum += numberrange[index];
+        }
+        avg = sum / (intuserinput);
+        System.out.println( "Sum: " + sum);
+    }
+}//end method main
--- FundamentalProblems/test.java
+++ FundamentalProblems/test.java
@@ -3,6 +3,6 @@ public class test
 {
     public static void main(String[] args){
         
-        repeated = new String(new char[10]).replace("\0", "hello?");
+        
     }
     }
