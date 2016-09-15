22794ef9f42f26ebd88a86789ffe7eccabbf3fc1
JJ Choi <JJ Choi>
2016-09-15 21:39:44 +0700

    Fixed fatal flaw.

--- BigProblem1/Histogram.java
+++ BigProblem1/Histogram.java
@@ -5,51 +5,67 @@ public class Histogram {
         String userinput = JOptionPane.showInputDialog( "Insert any number: "); //userinput number
         int intuserinput = Integer.parseInt( userinput ); //convert userinput to int
 
-        int [] numberrange = new int [100]; //defined first array
+        int [] numberrange = new int [intuserinput]; //defined first array
+        int [] numbercount = new int [100];//defined second array
+        
+        int sum = 0;
+        double avg = 0.0;
 
         for (int index = 0; index < intuserinput; index++) 
         //Generate numbers and put in array
         {
-            int numbergenerated = (int)(Math.random() * 100);
-            numberrange [numbergenerated] += 1;
+            numberrange[index] = (int)(Math.random() * 100);
+            numbercount [numberrange [ index ]] += 1;
+            sum += numberrange[index];
         }//end first for loop
+        avg = ((double)sum/intuserinput);
 
-        for (int index = 0; index < 100; index++) 
-        //Print array with correct #'#'
+        for (int index = 0; index < numbercount.length; index++) //Print array with correct #'#'
         {
             String hasnumbers = "";
-            while(numberrange[index] > 0)
+            while(numbercount[index] > 0)
             {
                 hasnumbers += "#";
-                numberrange[index]--;
+                numbercount[index]--;
             }
             System.out.println( index + ": " + hasnumbers);
         }//end second for loop
-
-        int max = 0, min = 99, avg = 0, sum = 0, mode = 0;
-
+        
+        int max = 0, min = 99;
         for (int index = 0; index < numberrange.length; index++)
         //finding the max and the min
         {
-            if ( numberrange[ index ] > 0 )
+            if ( numberrange[index] > 0)
+            //finding min and max and mode
             {
-                max = index;
-            }
-            if ( numberrange[ index ] < 0 )
-            {
-                min = index;
+                if ( numberrange[index] < min )
+                {
+                    min = numberrange[index];
+                }
+                if ( numberrange[index] > max )
+                {
+                    max = numberrange[index];
+                }
             }
         }
+
         System.out.println( "Maximum: " + max);
         System.out.println( "Minimum: " + min);
-        
-        for (int index = 0; index < numberrange.length; index++)
-        //finding the sum
-        {
-            sum += numberrange[index];
-        }
-        avg = sum / (intuserinput);
         System.out.println( "Sum: " + sum);
-        System.out.println( "Avg: " + avg);
+        System.out.println( "Average: " + avg);
+        System.out.println( "Mode(s): ");
+        
+        int mode = 0;
+        for (int index = 0; index < numbercount.length; index++)
+        {
+            if ( numbercount[index] > mode)
+            {
+                mode = numbercount[index];
+                if ( numbercount[index] == mode)
+                {
+                    System.out.print(index + " ");
+                }
+            }
+        }
     }
-}//end method main
+}
--- BigProblem1/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test
-{
-    public static void main ( String args [] ) 
-    {
-        int numbergenerated = (int) (Math.random() * 99);
-        System.out.println( numbergenerated );
-    }
-}
