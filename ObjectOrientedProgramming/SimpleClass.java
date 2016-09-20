408b78e0c5d7581f785e19b3620730e5a3190fed
cmis <cmis@cmis-hs03.(none)>
2016-09-20 11:55:04 +0700

    Started OOP.

---
+++ ObjectOrientedProgramming/SimpleClass.java
@@ -0,0 +1,12 @@
+
+public class SimpleClass
+{
+    // instance variables - replace the example below with your own
+    private int color = 5;
+
+    
+    public static void main(String[] args)
+    {
+        SimpleClass shoe1 = new SimpleClass();
+    }
+}
