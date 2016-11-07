5387a26c843c0c0c16aed9097fef3a9f90be0267
cmis <cmis@cmis-hs03.(none)>
2016-11-07 11:55:37 +0700

    Getting confused at at these numbers. The gird isn't printing like a normal grid. Origin is messed up.

--- 2D Arrays/Hello.java
+++ 2D Arrays/Hello.java
@@ -3,18 +3,16 @@ public class Hello {
         int [][] grid = new int [][] {{0,1,2,3,4}, {5,6,7,8,9}, {10,11,12,13,14}};
 
         for (int y = 0; y < grid.length; y++){
-            for(int x = 0; x < grid[y].length; x++){
+            for(int x = 0; x < grid.length; x++){
                 System.out.print(grid[y][x]);
             }             
         }
-
-        for (int y = 0; y < grid.length; y++){
-            for(int x = 0; x < grid[y].length; x++){
-                System.out.print(grid[x][y]);
+        System.out.println();
+        for (int y = 0; y < grid[0].length; y++){
+            System.out.println();
+            for(int x = 0; x < grid.length; x++){
+                System.out.println(grid[x][y]);
             } 
-        }
-        
+        }        
     }
 }
-
-
---
+++ 2D Arrays/Problem1.java
@@ -0,0 +1,22 @@
+public class Problem1
+{
+    public static void main ( String args[] ) {
+        int [][] grid = new int[3][4];
+
+        for (int y = 0; y < grid[0].length; y++){
+            for( int x = 0; x < grid.length; x++){
+                grid[x][y] = (int)(Math.random() * 9);
+                System.out.print(grid[x][y] + " ");
+            }// end inner for
+            System.out.println();
+        }// end outer for
+        
+         for (int y = 0; y < grid[0].length; y++){
+            for( int x = 0; x < grid.length; x++){
+                if (grid[x][y] == 5){
+                    System.out.println("There is a 5 in: " + "(" + y + "," + x + ")");
+                }
+            }// end inner for
+        }// end outer for
+    }// end method mian
+}// end class
