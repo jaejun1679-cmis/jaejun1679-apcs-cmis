ea2486415ed36b3e35d819742a42d80a16fa1f6f
jaejun1679-cmis <jaejun1679@cmis.ac.th>
2016-11-10 09:20:29 +0700

    Delete test.java

--- TestFile/test.java
+++
@@ -1,8 +0,0 @@
-public class test
-{ public static void main( String[] args ) 
-  { 
-      char uniChar = '\u039A';
-      
-      System.out.println( uniChar );
-    } // end method main
-  } // end classs
