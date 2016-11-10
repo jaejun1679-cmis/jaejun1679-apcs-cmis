c017ca58c656a6e6df3ee22dca71d16e80efad23
cmis <cmis@cmis-hs03.(none)>
2016-11-10 09:56:04 +0700

    What is alphabet incrementor???????????????????

--- 2D Arrays/Problem4.java
+++ 2D Arrays/Problem4.java
@@ -1,14 +1,26 @@
+import javax.swing.JOptionPane;
 public class Problem4
 {
     public static void main (String args[] )
     {
-        int[][] grid = new int[4][6];
+        int rowInput = Integer.parseInt(JOptionPane.showInputDialog("How many numbers of row would you like? "));
+        int colInput = Integer.parseInt(JOptionPane.showInputDialog("Well, how about columns?"));
 
-        for (int y = 0; y < grid.length; y++){
-            for(char alphabet = 'A'; alphabet <= 'Z';alphabet++) {
-                System.out.println(alphabet);
+        int [][] grid = new int [rowInput][colInput];
+        
+        for (int row = 0; row < grid.length; row++){
+            for (int col = 0; col < grid[0].length; col++){
+                for(int alphabetIndex = 65; alphabetIndex == alphabetIndex + 1;alphabetIndex++) {
+                    grid[row][col] = ((char)alphabetIndex);              
+                }
             }
         }
         
+        for (int row = 0; row < grid.length; row++){
+            for (int col = 0; col < grid[0].length; col ++){
+                System.out.print(grid[row][col]);
+            }
+            System.out.println();
+        }        
     }
 }
