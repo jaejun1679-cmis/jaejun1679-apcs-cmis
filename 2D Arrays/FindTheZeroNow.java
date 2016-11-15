2c1d74da2e6deedc57b3f877a7597ac93a01d565
cmis <cmis@cmis-hs03.(none)>
2016-11-15 08:16:41 +0700

    Finished 7. Have to do password thing.

--- 2D Arrays/FindTheZeroNow.java
+++ 2D Arrays/FindTheZeroNow.java
@@ -10,15 +10,16 @@ public class FindTheZeroNow{
             col = Integer.parseInt(JOptionPane.showInputDialog("Input the amount of columns: "));
         }
 
-        int [][] gameBoard = new int [row][col];
+        int [][] gameBoard = new int [row][col];//this is my gameBoard
+
         for (int y = 0; y < gameBoard.length; y++){
             for(int x = 0; x < gameBoard[0].length; x++){
                 gameBoard[y][x] = (int)(Math.random() * 100 + 1);
             }             
         }
 
-        int goalRow = (int)(Math.random() * (row - 1) + 1);
-        int goalCol = (int)(Math.random() * (col - 1) + 1);       
+        int goalRow = (int)(Math.random() * col); ;
+        int goalCol = (int)(Math.random() * row); ;
         gameBoard[goalRow][goalCol] = 0;
 
         System.out.println("Here is the game board.");
@@ -33,36 +34,39 @@ public class FindTheZeroNow{
 
         int guessRow = Integer.parseInt(JOptionPane.showInputDialog("Guess the row number: "));
         int guessCol = Integer.parseInt(JOptionPane.showInputDialog("Now guess the column number: "));
+        boolean test = true;
         int userguess = gameBoard[guessRow][guessCol];
 
-        while ( userguess != 0){
-            if ( userguess == 0){
-                System.out.println("Bring this man a cookie! You have found the zero!");
-                System.exit(0);
-            }
-            
-            System.out.println("Nope. Guess again!");
-            gameBoard[guessRow][guessCol] = 78;
-            
-            for (int y = 0; y < gameBoard[0].length; y++){
-                for(int x = 0; x < gameBoard.length; x++){
-                    if ( gameBoard[guessRow][guessCol] == 78){
-                        System.out.print((char)gameBoard[guessRow][guessCol]);
+        if ( userguess == 0){
+            System.out.println("Bring this man a cookie! You have found the zero!");
+            System.exit(0);
+        }
+        else {
+            while ( test ){
+                if ( userguess == 0){
+                    System.out.println("Bring this man a cookie! You have found the zero!");
+                    System.exit(0);
+                }
+                userguess = gameBoard[guessRow][guessCol];
+                System.out.println("Nope. Guess again!");
+                gameBoard[guessRow][guessCol] = 101;
+
+                for (int y = 0; y < gameBoard[0].length; y++){
+                    for(int x = 0; x < gameBoard.length; x++){
+                        if ( gameBoard[y][x] == 1000){
+                            System.out.print("N ");
+                        }
+                        else{
+                            System.out.print("X "); 
+                        }
                     }
-                    else{
-                       System.out.print("X "); 
-                    }
+                    System.out.println();
                 }
                 System.out.println();
+
+                guessRow = Integer.parseInt(JOptionPane.showInputDialog("Guess the row number again: "));
+                guessCol = Integer.parseInt(JOptionPane.showInputDialog("Now guess the column number once more: "));
             }
-            System.out.println();
-            
-            guessRow = Integer.parseInt(JOptionPane.showInputDialog("Guess the row number: "));
-            guessCol = Integer.parseInt(JOptionPane.showInputDialog("Now guess the column number: "));
         }
-        
-        
     }
 }
-
-//find out why it is printing all N after X. 
