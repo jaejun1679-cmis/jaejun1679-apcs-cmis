f70d686b0f83414311d73ff0d069b76dc13eaee9
JJ Choi <JJ Choi>
2016-11-16 20:00:30 +0700

    Warping up the problems.

--- 2D Arrays/PasswordProtector.java
+++ 2D Arrays/PasswordProtector.java
@@ -3,17 +3,30 @@ public class PasswordProtector{
     public static void main ( String args [] ) {
         String lastName = JOptionPane.showInputDialog("What is thy nameth? ");
         String lastNameLowerCase = lastName.toLowerCase();
+        char[] lastNameLowerCaseArray = lastNameLowerCase.toCharArray();
         
-        //char [][] nameDetails = new char [2][lastName.length()];
+        char firstLetter = lastNameLowerCaseArray[0];
+        char secondLetter = lastNameLowerCaseArray[1];
         
-        char[] lastNameArray = lastName.toCharArray();
-        char[] lastNameLowerCaseArray = lastNameLowerCase.toCharArray(); //can I merge these two?
+        boolean start = true; 
+        boolean stop = true;
+        
+        while (start){
+            String userPassword = JOptionPane.showInputDialog("How about thy password'th thy shalt use? ");
+            String userPasswordLowerCase = userPassword.toLowerCase();
+            
+        }
         
         
-        String userPassword = JOptionPane.showInputDialog("How about thy password'th thy shalt use? ");
+        
     }
 }
 
 //find out how to mix the two array
 //start comparing the userPassword and the two arrays
 //use index wisely to pinpoint similarities within the password and the name
+
+/*for(int index = 0; index < lastNameArray.length; index++ )
+{
+System.out.println(lastNameArray[index]);
+}*/ // for testing purposes
