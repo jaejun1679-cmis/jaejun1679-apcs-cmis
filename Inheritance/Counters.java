2176ffcbbe3785478488fdf58aa4ec276e565977
jaejun1679-cmis <jaejun1679@cmis.ac.th>
2016-11-25 07:46:27 +0700

    Delete Counters.java

--- Inheritance/Counters.java
+++
@@ -1,9 +0,0 @@
-public class Counters extends Furniture
-{
-    String shade; 
-    
-    public Counter(String type, String wood, String shade){
-        super(type,wood);
-        this.shade = shade;
-    }
-}
