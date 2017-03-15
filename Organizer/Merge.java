2e5542b431ecfb15342695cd178188af65c32262
cmis <cmis@cmis-hs07.(none)>
2017-03-15 08:41:03 +0700

    Progess

--- Organizer/Merge.java
+++ Organizer/Merge.java
@@ -2,40 +2,54 @@ import java.lang.*;
 public class Merge
 {
     public static void main( String[] args ) {
-
+        int[] hello = new int[10];
+        for(int index = 0; index < hello.length; index++) {
+            hello[index] = (int)(Math.random() * 9 + 1);
+        }
+        for(int index = 0; index < hello.length; index++ ) {
+            System.out.print(hello[index]);
+        }
+        //MergeSplit(hello);
     }
 
     public static int[][] Split(int[] nums) {
-        int[] left; 
-        int[] right;
-        int numsLength = nums.length;
-
-        if(numsLength % 2 == 0) {
-            for(int index = 0; index < nums.length; index++) {
-                if(index % 2 == 0) {
-                    left[index] = nums[index];
-                } else {
-                    right[index] = nums[index];
-                }
+        int length = nums.length;
+        int [] left;
+        int [] right;
+        if(nums.length % 2 == 0 ) {
+            left = new int[(length/2)]; 
+            right = new int[(length/2)];
+            
+             for(int index = 0; index < (length/2); index++) {
+                left[index] = nums[index];
+            }for(int index = (length/2); index < length; index++) {
+                right[index] = nums[index];
             }
         } else {
-            for(int index = 0; index < nums.length; index++) {
-                if(index % 2 == 0) {
-                    left[index] = nums[index];
-                } else {
-                    right[index] = nums[index];
-                }
+            left = new int[length / 2]; 
+            right = new int[(length / 2) + 1];
+            
+             for(int index = 0; index < (length/2); index++) {
+                left[index] = nums[index];
+            }for(int index = (length/2); index < length; index++) {
+                right[index] = nums[index];
             }
         }
-        int[][] asd = new int[][] {left, right};
-        return asd;
+        
+        int[][] returnArrays = new int[][] {left, right};
+        return returnArrays;
     }
 
     public int[] Merge() {
-
+        int[] rocks = new int[5]; //place holder
+        return rocks;
     }
 
-    public int[] MergeSplit() {
-
+    public int[] MergeSplit(int[] nums) {
+        if(nums.length == 1) {
+            return nums;
+        }
+        
+        return nums;
     }
 }
